2a10003aa9ed5ad15c1a1c143ac2f88da412d81c
Pablo Baleztena <pablo@ultharsoftware.com>
2020-11-02 19:03:00 -0300

    clase-14

---
+++ modulo-4/clase-14/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
