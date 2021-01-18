ad116f71f468828a3bfb022650e3ec60dc76e1ff
Pablo Baleztena <pablo@ultharsoftware.com>
2021-01-18 18:59:05 -0300

    clase 31

---
+++ modulo-6/clase-31/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.listen(3000);
+
+//Middlewares
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.urlencoded());
+
+//Routes
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
