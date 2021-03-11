fdf1974ff4a46a03e589158c6efbe8279a1d1ce8
Pablo Baleztena <pablo@ultharsoftware.com>
2021-03-11 19:15:34 -0300

    clase 44

--- modulo-9/clase-42/admin-dashboard/src/App.js
+++ modulo-9/clase-42/admin-dashboard/src/App.js
@@ -1,15 +1,62 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import SideMenu from "./components/side-menu/SideMenu";
+import DataCardSmall from "./components/data-cards/data-card-small/DataCardSmall";
+import DataCardBig from "./components/data-cards/data-card-big/DataCardBig";
 
 function App() {
+    const smallCardsValue = [
+        {
+            title: "Products in Data Base",
+            value: "135",
+            color: "primary",
+            icon: "fa-clipboard-list",
+        },
+        {
+            title: "Amount in products",
+            value: "$546.456",
+            color: "success",
+            icon: "fa-dollar-sign",
+        },
+        {
+            title: "Users quantity",
+            value: "38",
+            color: "warning",
+            icon: "fa-user-check",
+        },
+    ];
+
     return (
         <div className="App">
             <div id="wrapper">
                 <SideMenu />
-                <div id="content-wrapper" class="d-flex flex-column">
+                <div id="content-wrapper" className="d-flex flex-column">
                     <div id="content">
                         <Header />
+                        <div className="container-fluid">
+                            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                                <h1 className="h3 mb-0 text-gray-800">
+                                    App Dashboard
+                                </h1>
+                            </div>
+                            <div className="row">
+                                {smallCardsValue.map((elem, index) => {
+                                    return (
+                                        <DataCardSmall
+                                            key={index}
+                                            title={elem.title}
+                                            value={elem.value}
+                                            icon={elem.icon}
+                                            color={elem.color}
+                                        />
+                                    );
+                                })}
+                            </div>
+                            <div className="row">
+                                <DataCardBig />
+                                <DataCardBig />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ modulo-9/clase-42/admin-dashboard/src/components/data-cards/data-card-small/DataCardSmall.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+export default function DataCardSmall(props) {
+    return (
+        <div className="col-md-4 mb-4">
+            <div
+                className={`card border-left-${props.color} shadow h-100 py-2`}
+            >
+                <div className="card-body">
+                    <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                            <div
+                                className={`text-xs font-weight-bold text-${props.color} text-uppercase mb-1`}
+                            >
+                                {props.title}
+                            </div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">
+                                {props.value}
+                            </div>
+                        </div>
+                        <div className="col-auto">
+                            <i
+                                className={`fas ${props.icon} fa-2x text-gray-300`}
+                            ></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+DataCardSmall.propTypes = {
+    color: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+};
+
+DataCardSmall.defaultProps = {
+    color: "primary",
+};
